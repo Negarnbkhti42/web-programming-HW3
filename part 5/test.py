a08b85f7b08945e8ca9be3e193dc7f1ef27a5783
Negarnbkhti42 <norob35@gmail.com>
2023-02-18 00:41:35 +0330

    Add test

---
+++ part 5/test.py
@@ -0,0 +1,37 @@
+from locust import HttpUser, between, task
+import uuid
+import random
+
+hash_to_string = {}
+hashes = []
+
+
+class WebsiteUser(HttpUser):
+    # host = "http://213.233.179.83"
+    host = "http://localhost:3000"
+    token = None
+
+    @task(1)
+    def addUser(self):
+        result = self.client.post(
+            "/sign_up",
+            json={"email": "efpyi@example.com",
+                  "password": "123",
+                  "firstName": "shahab",
+                  "lastName": "hoseeini",
+                  "gender": "M",
+                  "phoneNumber": "09161234123"
+                  }).json()
+
+        assert result["token"] is not None
+        token = result["token"]
+
+        @task(2)
+        def getCities(self):
+            result = self.client.get("/cities")
+            assert result.status_code == 200
+
+        @task(0)
+        def logOut(self):
+            result = self.client.post("/logout", headers={"Token": token})
+            assert result.status_code == 200
